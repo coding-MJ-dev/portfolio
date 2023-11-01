a7d68e38fde9d5663f16c532e8a579e8c9a39c10
MJ-rocket <127004932+MJ-rocket@users.noreply.github.com>
2023-11-01 13:31:06 +1100

    JS-home

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
-
+// ---- HEADER ----
 // Find haeder
 const header = document.querySelector('.header');
-
 // define the height of the header
 const headerHeight = header.getBoundingClientRect().height;
 
 
  // when scrolling down, if Y axix > headerHeight, I will change the style from transparent to dark
 document.addEventListener('scroll', () => {
-  console.log(window.scrollY);
+  //console.log(window.scrollY);
   if (window.scrollY > headerHeight) {
     header.classList.add('header--dark');
   } else {
@@ -17,3 +16,10 @@ document.addEventListener('scroll', () => {
 })
 
 
+// ---- HOME ----
+// make the home contents transparent when scroll down
+const home = document.querySelector('.home__content');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+})
