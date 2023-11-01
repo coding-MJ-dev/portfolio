927804143070d0e30fe63c4cd1af761b2dd33785
MJ-rocket <127004932+MJ-rocket@users.noreply.github.com>
2023-11-02 10:01:21 +1100

    JS-projects

--- src/projects.js
+++ src/projects.js
@@ -2,11 +2,27 @@
 
 const categories = document.querySelector('.categories');
 const projects = document.querySelectorAll('.project');
+const projectsContainer = document.querySelector('.projects');
 categories.addEventListener('click', (event) => {
   const filter = event.target.dataset.filter;
   if (filter == null) {
     return;
   }
+  manageActiveSelection(event.target);
+  filterProjects(filter);
+});
+
+
+//show the active menu
+function manageActiveSelection(target) {
+  const active = document.querySelector('.category--selected');
+  active.classList.remove('category--selected');
+  target.classList.add('category--selected');
+}
+
+
+function filterProjects(filter) {
+  //project filtering
   projects.forEach(project => {
     console.log(project.dataset.filter)
     if (filter === 'all' || filter === project.dataset.filter) {
@@ -14,6 +30,9 @@ categories.addEventListener('click', (event) => {
     } else {
       project.style.display = 'none';
     }
-  })
-
-})
+  });
+  projectsContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projectsContainer.classList.remove('anim-out')
+  }, 250)
+}
