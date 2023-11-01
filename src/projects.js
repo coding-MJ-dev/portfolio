9e266870e9232f552436006b69556b1b865aeb69
MJ-rocket <127004932+MJ-rocket@users.noreply.github.com>
2023-11-01 16:56:20 +1100

    JS-filter

--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const categories = document.querySelector('.categories');
+const projects = document.querySelectorAll('.project');
+categories.addEventListener('click', (event) => {
+  const filter = event.target.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+  projects.forEach(project => {
+    console.log(project.dataset.filter)
+    if (filter === 'all' || filter === project.dataset.filter) {
+      project.style.display = 'block';
+    } else {
+      project.style.display = 'none';
+    }
+  })
+
+})
