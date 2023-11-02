8461a295a61fcdca320299701c33963d909ec372
MJ-rocket <127004932+MJ-rocket@users.noreply.github.com>
2023-11-02 16:32:48 +1100

    JS-Observer2

--- src/active_menu.js
+++ src/active_menu.js
@@ -6,8 +6,13 @@ const navItems = sectionIds.map(id => document.querySelector(`[href="${id}"]`));
 
 const visibleSections = sectionIds.map(() => false);
 
+let activeNavItem = navItems[0];
 
-const options = {};
+const options = {
+  rootMargin: '-20% 0px 0px 0px',
+  threshold: [0, 0.98],
+};
+
 const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach(section => observer.observe(section));
 
@@ -21,16 +26,22 @@ function observerCallback(entries) {
       entry.isIntersecting &&
       entry.intersectionRatio >= 0.99;
   })
-  console.log(visibleSections);
-  console.log(selectLastOne);
 
   const navIndex = selectLastOne ? sectionIds.length - 1 : findFirstIntersecting(visibleSections);
 
-  console.log(navIndex)
-  
+  selectNavItem(navIndex);
 }
 
 function findFirstIntersecting(sections) {
   const index = sections.indexOf(true);
   return index >= 0 ? index : 0;
+}
+
+
+function selectNavItem(index) {
+  const navItem = navItems[index];
+  if (!navItem) return;
+  activeNavItem.classList.remove('active');
+  activeNavItem = navItem;
+  activeNavItem.classList.add('active');
 }
