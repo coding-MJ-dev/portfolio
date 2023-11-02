892c544ca1883f893c488a3b1f7f79350b2f5164
MJ-rocket <127004932+MJ-rocket@users.noreply.github.com>
2023-11-02 14:42:18 +1100

    JS-Osever1

---
+++ src/active_menu.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const sectionIds = ['#home', '#about', '#skills',  '#work', '#testimonials', '#contact']
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id => document.querySelector(`[href="${id}"]`)); 
+
+const visibleSections = sectionIds.map(() => false);
+
+
+const options = {};
+const observer = new IntersectionObserver(observerCallback, options);
+sections.forEach(section => observer.observe(section));
+
+function observerCallback(entries) {
+  let selectLastOne; //flag
+  entries.forEach(entry => {
+    const index = sectionIds.indexOf(`#${entry.target.id}`);
+    visibleSections[index] = entry.isIntersecting;
+    selectLastOne =
+      index === sectionIds.length - 1 &&
+      entry.isIntersecting &&
+      entry.intersectionRatio >= 0.99;
+  })
+  console.log(visibleSections);
+  console.log(selectLastOne);
+
+  const navIndex = selectLastOne ? sectionIds.length - 1 : findFirstIntersecting(visibleSections);
+
+  console.log(navIndex)
+  
+}
+
+function findFirstIntersecting(sections) {
+  const index = sections.indexOf(true);
+  return index >= 0 ? index : 0;
+}
